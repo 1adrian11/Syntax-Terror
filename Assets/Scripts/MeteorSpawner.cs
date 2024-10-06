fd9c96acca56721c91ccd00a3cfd255d0e7260b9
1adrian11 <adrianosz340@gmail.com>
2024-10-06 19:37:27 +0200

    more meteors

--- Assets/Scripts/MeteorSpawner.cs
+++ Assets/Scripts/MeteorSpawner.cs
@@ -6,15 +6,26 @@ using UnityEngine.Animations;
 
 public class MeteorSpawner : MonoBehaviour
 {
-    public float spawnrate;  //milyen gyorsan spawnol
-    public Transform spawnpoint;
+    public float spawnrate;  // milyen gyorsan spawnol
     public float lastspwan;
-    public float minrot, maxrot;  //forgas gyorsasaga
+    public float minrot, maxrot;  // forgás gyorsasága
+
+    public Transform minspawnpoint, maxspawnpoint;
+
     public Vector2 minspeed, maxspeed; 
-    public MeteorControll Template;
+    public Vector2 minspawnvector => minspawnpoint.position;
+    public Vector2 maxspawnvector => maxspawnpoint.position;
+
+    public MeteorControll Template1, Template2, Template3, Template4;
+
+    private List<MeteorControll> templates; // List to hold templates
+
     void Start()
     {
         lastspwan = Time.time;
+
+        // Add templates to the list
+        templates = new List<MeteorControll> { Template1, Template2, Template3, Template4 };
     }
 
     void Update()
@@ -25,15 +36,26 @@ public class MeteorSpawner : MonoBehaviour
         }
     }
 
-    private void spawn(){
-        MeteorControll ac = Instantiate(Template);
-        ac.transform.position = spawnpoint.position;
+    private void spawn()
+    {
+        // Select a random template
+        MeteorControll selectedTemplate = templates[Random.Range(0, templates.Count)];
+        
+        // Instantiate the selected template
+        MeteorControll ac = Instantiate(selectedTemplate);
+        
+        Vector2 spawnpoint = new Vector2(Random.Range(minspawnpoint.position.x, maxspawnpoint.position.x), minspawnpoint.position.y);
+        
+        // 2 pont között random megjelenés
+        ac.transform.position = spawnpoint;
         ac.RotationSpeed = Random.Range(minrot, maxrot);
-        ac.Speed = new (Random.Range(minspeed.x, maxspeed.x), Random.Range(minspeed.y, maxspeed.y));  //milyen gyorsan jojjenek 
+        ac.Speed = new Vector2(Random.Range(minspeed.x, maxspeed.x), Random.Range(minspeed.y, maxspeed.y));  // Milyen gyorsan jöjjenek 
+        
         lastspwan = Time.time;
     }
 
-    private bool ready(){
+    private bool ready()
+    {
         return Time.time > (lastspwan + spawnrate);
     }
 }
