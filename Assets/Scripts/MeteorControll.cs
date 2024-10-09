393f6800402f776ced5f8ac5ae5b6fc0b294f2d9
1adrian11 <adrianosz340@gmail.com>
2024-10-10 00:04:16 +0200

    meteor spawn after kill
    nagy meteor pusztulásakor két kisebb jön létre helyette, egy helyett.

--- Assets/Scripts/MeteorControll.cs
+++ Assets/Scripts/MeteorControll.cs
@@ -6,42 +6,52 @@ using UnityEngine;
 
 public class MeteorControll : MonoBehaviour
 {
-    public float RotationSpeed;  //forgas sebessege
-    public Vector2 Speed; //mozgas
+    public float RotationSpeed;  // forgás sebessége
+    public Vector2 Speed; // mozgás sebessége
     public MeteorControll SpawnAfterKill;
 
     void Update(){
         RotMeteor();
         MovMeteor();
     }
-    private void OnTriggerEnter2D(Collider2D other) {    //lovedekkel utkozes
+
+    private void OnTriggerEnter2D(Collider2D other) {    // lövedékkel ütközés
         ShootControll Bullet = other.GetComponent<ShootControll>();
         if(Bullet != null) {
             OnHit(Bullet);
         }
     }
 
-    private void OnHit (ShootControll bullet) {
+    private void OnHit(ShootControll bullet) {
         Destroy(bullet.gameObject);
+
         if (SpawnAfterKill != null){
-            MeteorControll i = Instantiate(SpawnAfterKill);
-            i.transform.position = this.transform.position;
-            i.RotationSpeed = RotationSpeed;
-            i.Speed = Speed;
+            // Első meteor létrehozása az eredeti sebességgel
+            MeteorControll meteor1 = Instantiate(SpawnAfterKill);
+            meteor1.transform.position = this.transform.position;
+            meteor1.RotationSpeed = RotationSpeed;
+            meteor1.Speed = Speed; // eredeti sebesség
+
+            // Második meteor létrehozása, az X sebesség negálva
+            MeteorControll meteor2 = Instantiate(SpawnAfterKill);
+            meteor2.transform.position = this.transform.position;
+            meteor2.RotationSpeed = RotationSpeed;
+            meteor2.Speed = new Vector2(-Speed.x, Speed.y); // X koordináta negált
+
         }
 
-        Destroy(this.gameObject);
+        Destroy(this.gameObject); // az eredeti meteor megsemmisítése
     }
 
     private void RotMeteor(){
         float rot_z = transform.rotation.eulerAngles.z + (RotationSpeed * Time.deltaTime);
-        Vector3 rot_r = new (0, 0, rot_z);
+        Vector3 rot_r = new Vector3(0, 0, rot_z);
         transform.rotation = Quaternion.Euler(rot_r);
     }
 
-    public void MovMeteor (){
-        float new_x = transform.position.x + (Speed.x*Time.deltaTime);
-        float new_y = transform.position.y + (Speed.y*Time.deltaTime);
+    public void MovMeteor () {
+        float new_x = transform.position.x + (Speed.x * Time.deltaTime);
+        float new_y = transform.position.y + (Speed.y * Time.deltaTime);
         transform.position = new Vector2(new_x, new_y);
     }
 }
